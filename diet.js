3433d8c569878603283ff611c58907bec09a2919
Arshdeep0855 <Arshdeep0106.becse24@chitkara.edu.in>
2025-04-18 11:11:19 +0530

    Changes in design

---
+++ diet.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('calorieForm');
+    const result = document.getElementById('calorieResult');
+  
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const weight = parseFloat(document.getElementById('weight').value);
+      const height = parseFloat(document.getElementById('height').value);
+      const age = parseInt(document.getElementById('age').value);
+      const activity = parseFloat(document.getElementById('activity').value);
+  
+      const bmr = 10 * weight + 6.25 * height - 5 * age + 5; // Mifflin-St Jeor (Male)
+      const calories = Math.round(bmr * activity);
+  
+      result.innerHTML = `Your estimated daily calories: <span class="text-danger fw-bold">${calories}</span> kcal`;
+    });
+  });
+  
